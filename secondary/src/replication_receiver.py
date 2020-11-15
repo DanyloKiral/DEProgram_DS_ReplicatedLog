275b1420979ffe5ced3270490427cb283ea6dd43
Danylo Kiral <kiral.danylo@gmail.com>
2020-11-15 15:02:42 +0200

    Added delay simulation for replication

--- secondary/src/replication_receiver.py
+++ secondary/src/replication_receiver.py
@@ -1,3 +1,5 @@
+import random
+import time
 from shared.replication_receiver_pb2 import ReplicationRequest, ReplicationResponse
 from shared import replication_receiver_pb2_grpc
 from di_container import ServiceProviders
@@ -9,5 +11,10 @@ class ReplicationReceiver(replication_receiver_pb2_grpc.ReplicationReceiverServi
         self.__message_service: MessageService = ServiceProviders.message_service_provider()
 
     def replicate_message(self, request: ReplicationRequest, context):
+        self.simulateDelay()
         self.__message_service.append(request.message)
         return ReplicationResponse(success=True)
+
+    def simulateDelay(self):
+        rand_delay_ms = random.randrange(500, 2000, 50)
+        time.sleep(rand_delay_ms / 1000)
